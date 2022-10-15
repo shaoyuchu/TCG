9cd35d41e84d505e0058a8db1b614e22c746cce5
shaoyu0966 <b06705028@ntu.edu.tw>
2022-10-16 00:11:37 +0800

    add board to visited

--- hw1_broken_puzzle/src/a_star_solver.cpp
+++ hw1_broken_puzzle/src/a_star_solver.cpp
@@ -1,5 +1,19 @@
 #include "a_star_solver.h"
 
+void PQWithRemove::replaceIfSmallerCost(Board& replaceBy) {
+    for (int i = c.size() - 1; i >= 0; i++) {
+        // no existance with higher cost
+        if (this->c[i].getEstTotalCost() <= replaceBy.getEstTotalCost()) return;
+
+        // remove if the same puzzle exists
+        if (this->c[i].toBitset() == replaceBy.toBitset()) {
+            this->c.erase(this->c.begin() + i);
+            this->push(replaceBy);
+            return;
+        }
+    }
+}
+
 AStarSolver::AStarSolver(Board& initialBoard) {
     this->priorityQueue.push(initialBoard);
     this->visited.insert(initialBoard.toBitset());
@@ -13,16 +27,17 @@ vector<pair<short, Action>> AStarSolver::solve() {
     while (!this->priorityQueue.empty()) {
         Board curBoard = this->priorityQueue.top();
         this->priorityQueue.pop();
+        this->closedList.insert(curBoard.toBitset());
 
         // check if the game ends, if not, add curBoard to closed list
         if (curBoard.isCompleted()) return curBoard.getPrevMoves();
-        this->closedList.insert(curBoard.toBitset());
 
         // search deeper
         for (Board nextBoard : curBoard.getNext()) {
             if (nextBoard.isNull()) break;
             if (!this->isVisited(nextBoard)) {
                 this->priorityQueue.push(nextBoard);
+                this->visited.insert(nextBoard.toBitset());
             } else if (!this->isClosed(nextBoard)) {
                 this->priorityQueue.replaceIfSmallerCost(nextBoard);
             }
--- hw1_broken_puzzle/src/a_star_solver.h
+++ hw1_broken_puzzle/src/a_star_solver.h
@@ -15,19 +15,7 @@ class CostComparison {
 
 class PQWithRemove : public priority_queue<Board, vector<Board>, CostComparison> {
    public:
-    void replaceIfSmallerCost(Board& replaceBy) {
-        for (int i = c.size() - 1; i >= 0; i++) {
-            // no existance with higher cost
-            if (this->c[i].getEstTotalCost() <= replaceBy.getEstTotalCost()) return;
-
-            // remove if the same puzzle exists
-            if (this->c[i].toBitset() == replaceBy.toBitset()) {
-                this->c.erase(this->c.begin() + i);
-                this->push(replaceBy);
-                return;
-            }
-        }
-    }
+    void replaceIfSmallerCost(Board& replaceBy);
 };
 
 class AStarSolver : public PuzzleSolver {
